78ddf7d0ee2e78ae3c4c2525c640649c0fe6a72a
datmatrung <46807674+datmatrung@users.noreply.github.com>
2020-04-12 18:05:27 +0700

    Add files via upload

---
+++ XL_SanPham.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication13.Entity;
+using WebApplication13.DAL;
+
+namespace WebApplication13.Business
+{
+    public class XL_SanPham
+    {
+        public static List<SanPham> DocDanhSach(string tuKhoa = "")
+        {
+            return LT_SanPham.DocDanhSach(tuKhoa);
+        }
+
+        public static void ThemSanPham(SanPham sp)
+        {
+            LT_SanPham.ThemSanPham(sp);
+        }
+
+        public static bool XoaSanPham(int masp)
+        {
+            return LT_SanPham.XoaSanPham(masp);
+        }
+    }
+}
