da6af1d2299bdbc2687ffe306f1298da284e8679
datmatrung <46807674+datmatrung@users.noreply.github.com>
2020-05-07 12:27:55 +0700

    Add files via upload

---
+++ XL_DaGiac.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace WebApplication9
+{
+    public struct DaGiac
+    {
+        public Diem[] dsDiem;
+    }
+
+    public class XL_DaGiac
+    {
+        public static DaGiac KhoiTao(string s)
+        {
+            DaGiac kq;
+            var M = s.Split('|');
+            kq.dsDiem = new Diem[M.Length];
+            for (int i = 0; i < kq.dsDiem.Length; i++)
+            {
+                kq.dsDiem[i] = XL_Diem.KhoiTao(M[i]);
+            }
+            return kq;
+        }
+
+        public static double TinhChuVi(DaGiac A)
+        {
+            double kq = 0;
+            int N = A.dsDiem.Length;
+            for (int i = 0; i < N - 1; i++)
+            {
+                kq += XL_Diem.TinhKhoangCach(A.dsDiem[i], A.dsDiem[i+1]);
+            }
+            kq += XL_Diem.TinhKhoangCach(A.dsDiem[0], A.dsDiem[N-1]);
+            return kq;
+        }
+        
+        public static string xuat(DaGiac D)
+        {
+            string s = string.Empty;
+            for (int i = 0; i < D.dsDiem.Length; i++)
+            {
+                s += XL_Diem.xuat(D.dsDiem[i]) + ", ";
+            }
+            return s;
+        }
+    }
+}
---
+++ XL_TamGiac.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace WebApplication9
+{
+    public struct TamGiac
+    {
+        public Diem A;
+        public Diem B;
+        public Diem C;
+    }
+
+    public class XL_TamGiac
+    {
+        public static TamGiac KhoiTao(Diem A, Diem B, Diem C)
+        {
+            TamGiac kq;
+            kq.A = A;
+            kq.B = B;
+            kq.C = C;
+            return kq;
+        }
+
+        public static double TinhChuVi(TamGiac A)
+        {
+            double kq = 0;
+            kq += XL_Diem.TinhKhoangCach(A.A, A.B);
+            kq += XL_Diem.TinhKhoangCach(A.A, A.C);
+            kq += XL_Diem.TinhKhoangCach(A.B, A.C);
+            return kq;
+        }
+        
+        public static string Xuat(TamGiac T)
+        {
+            return $"({T.A.X},{T.A.Y}), ({T.B.X},{T.B.Y}), ({T.C.X},{T.C.Y})";
+        }
+    }
+}
