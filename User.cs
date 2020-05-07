c4178692e6767dc0c98c61d671bb29337e6d4154
datmatrung <46807674+datmatrung@users.noreply.github.com>
2020-05-07 13:00:46 +0700

    TaiKhoan

---
+++ User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication13.Entity
+{
+    public struct User
+    {
+        public int Id;
+        public string Username;
+        public string Password;
+    }
+}
---
+++ XL_User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication13.Entity;
+using WebApplication13.DAL;
+
+namespace WebApplication13.Business
+{
+    public class XL_User
+    {
+        public static bool AddUser(User user)
+        {
+            if (user.Password.Length < 8)
+            {
+                return false;
+            }
+            return LT_User.SaveUser(user);
+        }
+
+        public static bool dangNhap(User u)
+        {
+            List<User> ds = LT_User.LoadUsers();
+            bool kq = false;
+            foreach (var s in ds)
+            {
+                if ((s.Password == u.Password) && (s.Username == u.Username))
+                {
+                    kq = true;
+                    break;
+                }
+            }
+            return kq;
+        }
+    }
+}
